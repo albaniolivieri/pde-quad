2db8c29f629194b0778be855f9bb89a27f4611b5
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-09-29 16:45:11 +0200

    fix: small fixes

--- algorithm/quadratization.py
+++ algorithm/quadratization.py
@@ -3,33 +3,27 @@ from functools import reduce
 from sympy import *
 import numpy as np
 
-# maybe V + name_var --> dictionary
-def is_a_quadratization(V, deriv, name_var):
-    # turn into a dict too
-    V2, V2_names = [], []
-    for i in range(len(V)):
-        for j in range(len(V)):
-            mult = V[i]*V[j]
-            if mult not in V2:
-                V2.append(mult)
-                V2_names.append(name_var[i]*name_var[j])
-    
+def is_a_quadratization(V, deriv):
+    V2 = list(set((m1[0]*m2[0], m1[1]*m2[1]) for m1 in V for m2 in V))
+    V2_poly = list(term[1] for term in V2)
     quad = []
+    
     for pol in deriv:
-        if pol[1] not in V2:
-            result = is_linear_combination(V2, pol[1], V2_names)
+        if pol[1] not in V2_poly:
+            result = is_linear_combination(V2, pol[1])
             if not result: return False
-            quad.append(f"\n{pol[0]} = {result}")
+            quad.append(Eq(pol[0], result))
         else: 
-            quad.append(f"\n{pol[0]} = {V2_names[V2.index(pol[1])]}")
+            quad.append(Eq(pol[0], V2[V2_poly.index(pol[1])][0]))
     
     print("\nQuadratization:")
-    [print(new_expr) for new_expr in quad]
-    # to return quad but as polynomials
-    return True
+    for i in range(len(quad)):
+        pprint(exp(quad[i]))
+        #quad[i] = poly(quad[i])      
+    return quad
 
-def is_linear_combination(V2, der_pol, names_V2):
-    V2 = list(map(lambda p: (p, p.monoms(), p.coeffs()), V2))
+def is_linear_combination(V2_names, der_pol):
+    V2 = list(map(lambda p: (p[1], p[1].monoms(), p[1].coeffs()), V2_names))
     der_pol = (der_pol, der_pol.monoms(), der_pol.coeffs())
     [print("\nV2 poly", pol) for pol in V2]       
     
@@ -37,8 +31,7 @@ def is_linear_combination(V2, der_pol, names_V2):
     print(f"\nbase: {base}, length: {len(base)}\n")
     
     lambdas = symbols(["Lambda" + "_%d" % i for i in range(len(V2))])
-    subst_lambdas = list((coef, 0) for coef in lambdas)
-    
+    subst_lambdas = list((coef, 0) for coef in lambdas)   
     print(f"lambda set {lambdas}\n")
     
     print(f"derivative {der_pol[1]}\n")
@@ -75,32 +68,33 @@ def is_linear_combination(V2, der_pol, names_V2):
     der_expr = 0
     for i, s in enumerate(sols[0]):
         if s != 0: 
-            der_expr += s * names_V2[i]
-            print(f"{s} * {V2[i][0]}") 
-    print()        
+            der_expr += s * V2_names[i][0]
+            print(f"{s} * {V2_names[i][1]}")
     return simplify(der_expr)
     
 # Tests     
 u, ux, uxx, uxxx = symbols('u ux uxx uxxx')
 w0, w0x, w0xx, w0xxx = symbols('w0 w0x w0xx w0xxx')
+u_t, w_0t = symbols('u_t w_0t')
 
 # u_t = u**2 * uxx
 # w = u**2
 # w_t = 2u * (u**2 * uxx)
-V0 = list(map(lambda v: poly(v, [u, ux, uxx]), [1, u, ux, uxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx]))
-w0t = [("w_t", poly(2*u**3*uxx, [u, ux, uxx]))]
-assert is_a_quadratization(V0, w0t, [1, u, ux, uxx, w0, w0x, w0xx]) 
+V0 = list(map(lambda v, l: (l, poly(v, [u, ux, uxx])), [1, u, ux, uxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx], [1, u, ux, uxx, w0, w0x, w0xx]))
+w0t = [(w_0t, poly(2*u**3*uxx, [u, ux, uxx]))]
+assert is_a_quadratization(V0, w0t) == [Eq(w_0t, w0*w0xx - w0x**2/2)]
 
 # u_t = u * (ux**2 + u * uxx)
 # w = u**2
 # w_t = 2u**2 * (ux**2 + u * uxx)
-V1 = list(map(lambda v: poly(v, [u, ux, uxx]), [1, u, ux, uxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx]))
-w0t1 = [("u_t", poly(u*ux**2 + u**2*uxx, [u, ux, uxx])), ("w_t", poly(2*u**2*ux**2 + 2*u**3*uxx, [u, ux, uxx]))]
-assert is_a_quadratization(V1, w0t1, [1, u, ux, uxx, w0, w0x, w0xx]) 
+V1 = list(map(lambda v, l: (l, poly(v, [u, ux, uxx])), [1, u, ux, uxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx], [1, u, ux, uxx, w0, w0x, w0xx]))
+w0t1 = [(u_t, poly(u*ux**2 + u**2*uxx, [u, ux, uxx])), (w_0t, poly(2*u**2*ux**2 + 2*u**3*uxx, [u, ux, uxx]))]
+assert is_a_quadratization(V1, w0t1)#  == [Eq(u_t, u*w0xx/2), Eq(w_0t, w0*w0xx)]
 
 # u_t = u * (2 ux * uxx + u * uxxx + 1)
 # w = u**2
 # w_t = 2 * u**2 * (2 ux * uxx + u * uxxx + 1)
-V2 = list(map(lambda v: poly(v, [u, ux, uxx, uxxx]), [1, u, ux, uxx, uxxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx, 4*ux*uxx + 2*u*uxxx]))
-w0t2 = [("u_t", poly(u*(ux**2 + u*uxx + 1), [u, ux, uxx, uxxx])), ("w_t", poly(2*u**2*(2*ux*uxx + u*uxxx + 1), [u, ux, uxx, uxxx]))]
-assert is_a_quadratization(V2, w0t2, [1, u, ux, uxx, uxxx, w0, w0x, w0xx, w0xxx]) 
+V2 = list(map(lambda v, l: (l, poly(v, [u, ux, uxx, uxxx])), [1, u, ux, uxx, uxxx, u**2, 2*u*ux, 2*ux**2+2*u*uxx, 4*ux*uxx + 2*u*uxxx],
+    [1, u, ux, uxx, uxxx, w0, w0x, w0xx, w0xxx]))
+w0t2 = [(u_t, poly(u*(2*ux*uxx + u*uxxx + 1), [u, ux, uxx, uxxx])), (w_0t, poly(2*u**2*(2*ux*uxx + u*uxxx + 1), [u, ux, uxx, uxxx]))]
+assert is_a_quadratization(V2, w0t2) #== [Eq(u_t, ux*w0xx/2), Eq(w_0t, w0*w0xx + 2*w0)]
