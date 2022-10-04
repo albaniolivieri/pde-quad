3cba501f91bf01f353534545d23f102c7bfe9782
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-10-04 14:19:41 +0200

    feat: main function for quadratization

--- algorithm/check_quad.py
+++ algorithm/check_quad.py
@@ -1,23 +1,32 @@
-from quadratization import *
+from quadratization import is_a_quadratization
 from sympy import *
+from sympy import Derivative as D
 import numpy as np
 
-def get_quadratization(vars_func: tuple[list, sp.Eq], new_var, n_diff=1):
-    ux = vars_func[1]
-    w0 = symbols('w0')
-    if poly(ux.rhs.subs(new_var, w0)).total_degree() <= 2: 
-        diff_t = diff(new_var, t)
-        diff_x = dict()
-        for i in range(1, n_diff+1):
-            diff_x[f"deriv_{i}"] = diff(new_var, i)
-        V = list(diff_x.values()).append(new_var) + vars_func[0].append(1)
-        return is_a_quadratization(V, diff_t)
-    else: 
-        print("It is not possible to get a quadratization with this variable")
-        return False
+def get_quadratization(vars_func: tuple, var_indep, new_var, n_diff):
+    eq_t = vars_func[1]
+    wt = D(new_var, t).doit().subs(D(Function(vars_func[0][0])(var_indep[0], var_indep[1]), var_indep[0]), eq_t) 
     
+    refac = list(
+        (D(Function(vars_func[0][0])(var_indep[0], var_indep[1]), var_indep[1], len(vars_func[0][i])-1), symbols(vars_func[0][i]))
+        for i in range(1, len(vars_func[0])))
+    refac.append((Function(vars_func[0][0])(var_indep[0], var_indep[1]), symbols(vars_func[0][0])))
+    
+    deriv_x = list((symbols(f'wx{i}'), D(new_var, var_indep[1], i).doit().subs(refac)) for i in range(1, n_diff+1))
+        
+    new_vars = deriv_x + [(symbols('w'), new_var.subs(refac))]
+    vars = list(symbols(var) for var in vars_func[0])
+    
+    V = list((name, poly(exprs, vars)) for name, exprs in new_vars)
+    V.extend(list((var, poly(var, vars)) for var in vars) + [(1, poly(1, vars))])
+    
+    deriv_t = [(symbols(f'{vars_func[0][0]}t'), poly(eq_t.subs(refac), vars)), (symbols('wt'), poly(wt.subs(refac), vars))]
+    
+    return is_a_quadratization(V, deriv_t)
+
 #test 
-u, t, x = symbols('u t x')
-
-
-get_quadratization(([u, ux], ))
+t, x = symbols('t x')
+u = Function('u')(t,x)
+ut = u**2*D(u, x) + u
+w0 = u**2
+get_quadratization((['u', 'ux'], ut), (t,x), w0, 1)
