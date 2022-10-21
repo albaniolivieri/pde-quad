0e43fb95df4fc4c7629a75dd58da4c423d450b5b
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-10-21 15:00:32 +0200

    perf: efficiency improvements in quadratization

--- algorithm/quadratization_copy.py
+++ algorithm/quadratization_copy.py
@@ -27,8 +27,6 @@ def is_a_quadratization(V, deriv):
     return quad
 
 def reduce_set(V2):
-    print(len(V2))
-    ti = time.time()
     for i in range(len(V2)):
         for j in range(i):
             coef = V2[i][1].coeff_monomial(V2[j][2])
@@ -39,31 +37,27 @@ def reduce_set(V2):
         for j in range(i):
             coef = V2[j][1].coeff_monomial(V2[i][2])
             if coef != 0:
-               V2[j] = (V2[j][0] - coef * V2[i][0], V2[j][1] - coef * V2[i][1], V2[j][2])
-    print(time.time()-ti)                     
+               V2[j] = (V2[j][0] - coef * V2[i][0], V2[j][1] - coef * V2[i][1], V2[j][2])                  
     return [(a[0], a[1]) for a in V2]
 
-def is_linear_combination(V2, der_pol):          
+def is_linear_combination(V2, der_pol):
+    V2.sort(key=lambda pol: (pol[1].total_degree(), str(pol[0])), reverse=True)        
     der_expr = 0
-    for name_1, pol1 in V2:
-        subt = der_pol
-        if der_pol.LM() in pol1.monoms():
-            LM_subt = subt.LM()
-            LC_subt = subt.LC()
-            coef = pol1.coeff_monomial(LM_subt)
-            subt -= (LC_subt/coef)*pol1
-            der_expr += (LC_subt/coef)*name_1
-            if subt == 0:
-                return simplify(der_expr)
-            for name, pol2 in V2:
-                LM_subt = subt.LM()
-                LC_subt = subt.LC()
-                if LM_subt in pol2.monoms():
-                    coef = pol2.coeff_monomial(LM_subt)
-                    subt -= (LC_subt/coef)*pol2
-                    der_expr += (LC_subt/coef)*name
-                    if subt == 0:
-                        return simplify(der_expr)                 
+    for i in range(len(V2)):
+        subt = (der_pol, der_pol.LM())
+        coef = V2[i][1].coeff_monomial(subt[1])
+        if coef != 0:
+            LC_subt = subt[0].LC()
+            subt = (subt[0] - (LC_subt/coef)*V2[i][1], subt[1])
+            der_expr += (LC_subt/coef)*V2[i][0]
+            if subt[0] == 0: return simplify(der_expr)
+            for j in range(len(V2)):
+                subt = (subt[0], subt[0].LM())
+                coef = V2[j][1].coeff_monomial(subt[1])
+                if coef != 0:
+                    LC_subt = subt[0].LC()
+                    subt = (subt[0] - (LC_subt/coef)*V2[j][1], subt[1])
+                    der_expr += (LC_subt/coef)*V2[j][0]
+                    if subt[0] == 0: return simplify(der_expr)                 
     print("Not a quadratization")
-    return False            
-
+    return False    
