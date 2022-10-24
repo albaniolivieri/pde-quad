c09ba77325a7e76137261f586ae02dccbfa859a0
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-10-24 15:29:00 +0200

    perf: quadratization by pol reduction

--- algorithm/quadratization_copy.py
+++ algorithm/quadratization_copy.py
@@ -1,6 +1,5 @@
-from functools import reduce
-import time
 from sympy import *
+from utils import get_reduction
 
 def is_a_quadratization(V, deriv):
     V2 = list(set((m1[0] * m2[0], m1[1] * m2[1]) for m1 in V for m2 in V))
@@ -9,55 +8,37 @@ def is_a_quadratization(V, deriv):
         names.append(name)
         V2_poly.append(polyn)
     quad = []
-
     V2_red = reduce_set(V2)
     for name, pol in deriv:
         if pol not in V2_poly:
             result = is_linear_combination(V2_red, pol)
             if not result: return False
-            else: 
-                quad.append(Eq(name, result))
-        else: 
-            quad.append(Eq(name, names[V2_poly.index(pol)]))
-    
+            else: quad.append(Eq(name, result))
+        else: quad.append(Eq(name, names[V2_poly.index(pol)]))
     print("\nQuadratization:")
     for exprs in quad:
-        pprint(exprs)  
-           
+        pprint(exprs)       
     return quad
 
 def reduce_set(V2):
     for i in range(len(V2)):
         for j in range(i):
-            coef = V2[i][1].coeff_monomial(V2[j][2])
-            if coef != 0:
-                V2[i] = (V2[i][0] - coef * V2[j][0], V2[i][1] - coef * V2[j][1])
+            V2[i] = get_reduction(V2[i], V2[j])
         LC = V2[i][1].LC()
         V2[i] = (V2[i][0] / LC, V2[i][1] * (1 / LC), V2[i][1].LM())
         for j in range(i):
-            coef = V2[j][1].coeff_monomial(V2[i][2])
-            if coef != 0:
-               V2[j] = (V2[j][0] - coef * V2[i][0], V2[j][1] - coef * V2[i][1], V2[j][2])                  
+            V2[j] = get_reduction(V2[j], V2[i])                 
     return [(a[0], a[1]) for a in V2]
 
-def is_linear_combination(V2, der_pol):
-    V2.sort(key=lambda pol: (pol[1].total_degree(), str(pol[0])), reverse=True)        
-    der_expr = 0
+def is_linear_combination(V2, der_pol):  
+    der_tuple = (0, der_pol, der_pol.LM())
+    V2 = [(name, pol, pol.LM()) for name, pol in V2]   
     for i in range(len(V2)):
-        subt = (der_pol, der_pol.LM())
-        coef = V2[i][1].coeff_monomial(subt[1])
-        if coef != 0:
-            LC_subt = subt[0].LC()
-            subt = (subt[0] - (LC_subt/coef)*V2[i][1], subt[1])
-            der_expr += (LC_subt/coef)*V2[i][0]
-            if subt[0] == 0: return simplify(der_expr)
-            for j in range(len(V2)):
-                subt = (subt[0], subt[0].LM())
-                coef = V2[j][1].coeff_monomial(subt[1])
-                if coef != 0:
-                    LC_subt = subt[0].LC()
-                    subt = (subt[0] - (LC_subt/coef)*V2[j][1], subt[1])
-                    der_expr += (LC_subt/coef)*V2[j][0]
-                    if subt[0] == 0: return simplify(der_expr)                 
+        der_tuple = get_reduction(der_tuple, V2[i])
+        if der_tuple[1] == 0:
+            return simplify(-der_tuple[0])              
     print("Not a quadratization")
-    return False    
+    return False  
+
+
+    
--- algorithm/utils.py
+++ algorithm/utils.py
@@ -7,4 +7,12 @@ def get_order(set_derivs):
     for deriv in set_derivs:
         max_order = reduce(max, [der.args[1][1] for der in deriv.atoms(D)], max_order)
     return max_order
+
+def get_reduction(pol1, pol2):
+    coef = pol1[1].coeff_monomial(pol2[2])
+    if coef != 0:
+        new_pol = pol1[1] - coef * pol2[1]
+        return (pol1[0] - coef * pol2[0], new_pol, new_pol.LM())
+    else: 
+        return pol1
     
