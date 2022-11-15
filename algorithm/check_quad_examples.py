dfb2398ac5fc43174cca382ea1c25a570c6252d7
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-11-15 16:39:24 +0100

    files: new check quad for examples

---
+++ algorithm/check_quad_examples.py
@@ -0,0 +1,47 @@
+from sympy import *
+from sympy import Derivative as D
+from .quadratization_examples import is_a_quadratization
+from .utils import get_order
+from functools import reduce
+
+def get_quadratization(func_eq, new_vars: list, n_diff: int):
+    undef_fun = [symbol for symbol, _ in func_eq] 
+    x_var = [symbol for symbol in undef_fun[0].free_symbols if symbol != symbols('t')].pop()
+    
+    vars_t = [(f'w_{i}', new_vars[i]) for i in range(len(new_vars))] 
+    deriv_t = differentiate_t(func_eq, vars_t) + [(symbols(eqs[0].name + '_t'), eqs[1]) for eqs in func_eq] 
+    
+    quad_vars = differentiate_x(x_var, new_vars, n_diff)
+    max_order = max(get_order([der for _, der in deriv_t]), get_order([der for _, der in quad_vars]))
+
+    # ordering matters !
+    refac = []
+    for fun in undef_fun:
+        refac += [(D(fun, x_var, i), symbols(f'{fun.name}_{x_var}{i}')) 
+                  for i in range(max_order, 0, -1)] + [(fun, symbols(fun.name))]
+    
+    poly_vars = reduce(lambda name1, name2: name1 + name2[1].name + ',', refac, '')
+    ring_polys = ring(poly_vars, QQ) 
+    
+    V = [(name, ring_polys[0].ring_new(exprs.subs(refac))) for name, exprs in quad_vars]
+     
+    V.extend([(var, ring_polys[0].ring_new(var)) for _, var in refac + [(1, 1)]])  
+     
+    deriv_t = list(map(lambda exprs: (exprs[0], ring_polys[0].ring_new(exprs[1].subs(refac))), deriv_t))
+
+    return is_a_quadratization(V, deriv_t)
+
+def differentiate_t(funcs_eqs, new_vars):
+    deriv_t = []
+    refac = [(D(deriv[0], symbols('t')), deriv[1]) for deriv in funcs_eqs]
+    for i in range(len(new_vars)):
+        wt = D(new_vars[i][1], symbols('t')).doit().subs(refac)
+        deriv_t.append((symbols(f'{new_vars[i][0]}t'), wt.doit()))
+    return deriv_t
+
+def differentiate_x(var_indep, new_vars, n):
+    quad_vars = []
+    for i in range(len(new_vars)):
+        quad_vars.extend([(symbols(f'w_{i}{var_indep}{j}'), D(new_vars[i], var_indep, j).doit()) 
+                          for j in range(1, n + 1)] + [(symbols(f'w_{i}'), new_vars[i])])  
+    return quad_vars
---
+++ algorithm/quadratization_examples.py
@@ -0,0 +1,45 @@
+from sympy import *
+from .utils import reduction_sparse
+
+def is_a_quadratization(V, deriv):
+    V2 = list(set((m1[0] * m2[0], m1[1] * m2[1]) for m1 in V for m2 in V))
+    V2_poly, names = [], []
+    for name, polyn in V2: 
+        names.append(name)
+        V2_poly.append(polyn)
+    quad = []
+    NS = []
+    V2_red = reduce_set(V2)
+    for name, pol in deriv:
+        if pol not in V2_poly:
+            result = is_linear_combination(V2_red, pol)
+            if type(result) == tuple: NS.append(result[1])
+            else: quad.append(Eq(name, result))
+        else: quad.append(Eq(name, names[V2_poly.index(pol)]))
+    if NS != []: 
+        for pol in NS: pprint(pol)
+        return NS
+    print("\nQuadratization:")
+    for exprs in quad: pprint(exprs)       
+    return quad
+
+def reduce_set(V2):
+    for i in range(len(V2)):
+        for j in range(i):
+            V2[i] = reduction_sparse(V2[i], V2[j])
+        if V2[i][1] != 0:
+            LC = V2[i][1].coeff(V2[i][1].leading_monom())
+            V2[i] = (V2[i][0] / LC, V2[i][1] * (1 / LC), V2[i][1].leading_monom())
+            for j in range(i):
+                V2[j] = reduction_sparse(V2[j], V2[i])                 
+    return [(a[0], a[1]) for a in V2]
+
+def is_linear_combination(V2, der_pol):  
+    der_tuple = (0, der_pol, der_pol.leading_monom())
+    V2 = [(name, pol, pol.leading_monom()) for name, pol in V2]   
+    for i in range(len(V2)):
+        der_tuple = reduction_sparse(der_tuple, V2[i])
+        if der_tuple[1] == 0:
+            return simplify(-der_tuple[0])               
+    print("Not a quadratization")
+    return (False, der_tuple[0])  
