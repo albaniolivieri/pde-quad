f9421420e815ccd518ccb3e0730926ddb27a7c4a
Albani Olivieri <70730145+albaniolivieri@users.noreply.github.com>
2022-11-29 16:24:12 +0100

    fix: problem with max order in equations

--- algorithm/check_quad.py
+++ algorithm/check_quad.py
@@ -9,23 +9,26 @@ def get_quadratization(func_eq, new_vars: list, n_diff: int):
     undef_fun = [symbol for symbol, _ in func_eq] 
     x_var = [symbol for symbol in undef_fun[0].free_symbols if symbol != symbols('t')].pop()
     
-    new_vars_pol, poly_syms, eqs_pol = build_ring(func_eq, n_diff, new_vars, x_var)
+    max_order = get_order([expr for _, expr in func_eq])
     
-    dic_t, dic_x = get_dics(func_eq, poly_syms, eqs_pol, n_diff)
+    new_vars_pol, poly_syms, eqs_pol = build_ring(func_eq, n_diff, new_vars, x_var, max_order)
+    
+    dic_t, dic_x = get_dics(func_eq, poly_syms, eqs_pol, n_diff, max_order)
 
     return get_quad(dic_t, dic_x, new_vars_pol, eqs_pol, n_diff, x_var, poly_syms)
 
-def build_ring(func_eq, order, new_vars, var_indep):
-    # build ring for polys    
+def build_ring(func_eq, order, new_vars, var_indep, max_order):
+    # build ring for polys
+        
     refac = []
-    for fun, expr in func_eq:
+    for fun, _ in func_eq:
         refac += [(D(fun, var_indep, i), symbols(f'{fun.name}_{var_indep}{i}')) 
-                  for i in range(get_order([expr]), 0, -1)] + [(fun, symbols(fun.name))] 
+                  for i in range(max_order, 0, -1)] + [(fun, symbols(fun.name))] 
     
     poly_vars = []
-    for fun, eq in func_eq:
+    for fun, _ in func_eq:
         poly_vars.append(f'{fun.name}')
-        poly_vars.extend([f'{fun.name}_{var_indep}{i}' for i in range(1, get_order([eq]) + order + 1)])
+        poly_vars.extend([f'{fun.name}_{var_indep}{i}' for i in range(1, max_order + order + 1)])
         
     R, pol_sym = xring(poly_vars, QQ)
     print('ring:', R, pol_sym)
@@ -35,38 +38,29 @@ def build_ring(func_eq, order, new_vars, var_indep):
     
     return vars_pol, pol_sym, expr_pol
 
-def get_dics(func_eq, symb, eqs_pol, order): 
+def get_dics(func_eq, symb, eqs_pol, order, max_order): 
     # produce dict for derivatives 
     dic_x = {}
     dic_t = {}
     
-    offset = 0
-    for _, eq in func_eq:
-        max_order = get_order([eq]) + order
-        print('max_order', max_order)
-        for i in range(max_order): 
-            dic_x[symb[i + offset]] = symb[i + offset + 1]
-        offset += max_order + 1
+    der_order = max_order + order
     
-    offset = 0
-    k = 0
-    for _, eq in func_eq:
-        max_order = get_order([eq]) + order
-        for i in range(max_order): 
+    for j in range(len(func_eq)):
+        for i in range(der_order): 
+            dic_x[symb[i + (der_order+1)*j]] = symb[i + (der_order+1)*j + 1]
+    
+    for k in range(len(func_eq)):
+        for i in range(der_order - 1): 
             if i != 0:
-                dic_t[symb[i + offset]] = diff_dict(dic_t[symb[i - 1 + offset]], dic_x)
+                dic_t[symb[i + (der_order+1)*k]] = diff_dict(dic_t[symb[i - 1 + (der_order+1)*k]], dic_x)
             else: 
-                dic_t[symb[offset]] = eqs_pol[k][1]
-        offset += max_order + 1
-        k += 1
+                dic_t[symb[(der_order+1)*k]] = eqs_pol[k][1]
         
     return dic_t, dic_x
 
 def get_quad(dic_t, dic_x, new_vars, eqs_pol, order, var_indep, poly_syms):
     # get derivs
     # build V 
-    print('dic_t', dic_t)
-    print('dic_x', dic_x)
     new_vars_t, new_vars_x = differentiate_dict(dic_t, dic_x, new_vars, order, var_indep)
     
     deriv_t = new_vars_t + eqs_pol
@@ -89,18 +83,3 @@ def differentiate_dict(dic_t, dic_x, new_vars, order, var_indep):
             deriv_x.append((symbols(f'{name}{var_indep}{i}'), diff_dict(expr[1], dic_x, i)))
             
     return deriv_t, deriv_x
-
-def differentiate_t(funcs_eqs, new_vars):
-    deriv_t = []
-    refac = [(D(deriv[0], symbols('t')), deriv[1]) for deriv in funcs_eqs]
-    for i in range(len(new_vars)):
-        wt = D(new_vars[i][1], symbols('t')).doit().subs(refac)
-        deriv_t.append((symbols(f'{new_vars[i][0]}t'), wt.doit()))
-    return deriv_t
-
-def differentiate_x(var_indep, new_vars, n):
-    quad_vars = []
-    for i in range(len(new_vars)):
-        quad_vars.extend([(symbols(f'w_{i}{var_indep}{j}'), D(new_vars[i], var_indep, j).doit()) 
-                          for j in range(1, n + 1)] + [(symbols(f'w_{i}'), new_vars[i])])  
-    return quad_vars
--- tests/test_check_quad.py
+++ tests/test_check_quad.py
@@ -5,13 +5,27 @@ sys.path.append("..")
 from algorithm import check_quad as quad
 from algorithm.utils import get_order
 
+def differentiate_t(funcs_eqs, new_vars):
+    deriv_t = []
+    refac = [(D(deriv[0], symbols('t')), deriv[1]) for deriv in funcs_eqs]
+    for i in range(len(new_vars)):
+        wt = D(new_vars[i][1], symbols('t')).doit().subs(refac)
+        deriv_t.append((symbols(f'{new_vars[i][0]}t'), wt.doit()))
+    return deriv_t
+
+def differentiate_x(var_indep, new_vars, n):
+    quad_vars = []
+    for i in range(len(new_vars)):
+        quad_vars.extend([(symbols(f'w_{i}{var_indep}{j}'), D(new_vars[i], var_indep, j).doit()) 
+                          for j in range(1, n + 1)] + [(symbols(f'w_{i}'), new_vars[i])])  
+    return quad_vars
 
 def test_quad(func_eq, new_vars: list, n_diff: int):
     x_var = [symbol for symbol in func_eq[0][0].free_symbols if symbol != symbols('t')].pop()
     var_dic = [(symbols(f'w_{i}'), new_vars[i]) for i in range(len(new_vars))] 
-    quad_vars = quad.differentiate_x(x_var, new_vars, n_diff)
+    quad_vars = differentiate_x(x_var, new_vars, n_diff)
     undef_fun = [symbol for symbol, _ in func_eq]
-    deriv_t = quad.differentiate_t(func_eq, var_dic) + [(symbols(eqs[0].name + '_t'), eqs[1]) for eqs in func_eq] 
+    deriv_t = differentiate_t(func_eq, var_dic) + [(symbols(eqs[0].name + '_t'), eqs[1]) for eqs in func_eq] 
     max_order = max(get_order([der for _, der in deriv_t]), get_order([der for _, der in quad_vars]))
     
     refac = []
@@ -36,34 +50,38 @@ def test_quad(func_eq, new_vars: list, n_diff: int):
 t, x = symbols('t x')
 u = Function('u')(t,x)
 
+tests = []
+
 # u_t = u**2 * ux + u
 # w = u**2
 # w_t = 2u * (u**2 * ux + u)
 ut1 = u**2*D(u, x) + u
 w01 = u**2
-
-print(test_quad([(u, ut1)], [w01], 1))
+tests.append(test_quad([(u, ut1)], [w01], 1))
 
 ut2 = u**2*D(u, x, 2)
 w02 = u**2
-print(test_quad([(u, ut2)], [w02], 2))
+tests.append(test_quad([(u, ut2)], [w02], 2))
 
 ut3 = u * (D(u, x)**2 + u * D(u, x, 2))
 w03 = u**2
-print(test_quad([(u, ut3)], [w03], 2))
+tests.append(test_quad([(u, ut3)], [w03], 2))
 
 ut4 = u * (3 * D(u, x) * D(u, x, 2) + u * D(u, x, 3) + 1)
 w04 = u**2
-print(test_quad([(u, ut4)], [w04], 3))
+tests.append(test_quad([(u, ut4)], [w04], 3))
 
 ut5 = u**3 * D(u, x, 3)
-print(test_quad([(u, ut5)], [u**3, u * D(u, x)**2], 3))
+tests.append(test_quad([(u, ut5)], [u**3, u * D(u, x)**2], 3))
 
 u1 = Function('u1')(t,x)
 u1t = u1**3 * D(u1, x, 1)
 ut5 = u**3 * D(u, x, 3)
-print(test_quad([(u, ut5), (u1, u1t)], [u**3, u * D(u, x)**2, u1**3], 3))
+tests.append(test_quad([(u, ut5), (u1, u1t)], [u**3, u * D(u, x)**2, u1**3], 3))
 
+# Summary
+print('\nTests passed: ', tests.count(True))
+print('Tests failed: ', tests.count(False))
 
 
     
